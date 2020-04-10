441408cf7c705d0cdd86434e135626ca0073d3ca
makame <makame@mail.ru>
2020-04-10 14:14:12 +0200

    optimize footer

--- src/app/FooterText.tsx
+++ src/app/FooterText.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 
-const FooterText = (props: { year?: any; name?: any; desc?: any }) => (
+const FooterText = () => (
   <React.Fragment>
-    (C) {props.year} All Rights Reserved. This is the &quot;{props.name}&quot;
-    built with {props.desc}. Designed and implemented by{" "}
+    Designed and implemented by{" "}
     <a
       href="https://github.com/makamekm"
       target="_blank"
@@ -14,11 +13,5 @@ const FooterText = (props: { year?: any; name?: any; desc?: any }) => (
     </a>
   </React.Fragment>
 );
-
-FooterText.defaultProps = {
-  year: "2018",
-  name: "Admin Theme",
-  desc: "Bootstrap 4, React 16 (latest) & NPM",
-};
 
 export { FooterText };
