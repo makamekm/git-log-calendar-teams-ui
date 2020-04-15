30b6a4a2ba0945f3f0557ba501cff67facf28fab
makame <makame@mail.ru>
2020-04-15 11:21:35 +0200

    add env config

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const { override, addWebpackAlias } = require("customize-cra");
 
+const mode = process.env.NODE_ENV === "development" ? "dev" : "prod";
+
 module.exports = override(
   addWebpackAlias({
+    "@env": path.resolve(__dirname, "src/env", mode),
     "~": path.resolve(__dirname, "src/"),
   })
 );
---
+++ src/env/prod/config.tsx
@@ -0,0 +1,1 @@
+export const CONFIG_PIN = "12345";
