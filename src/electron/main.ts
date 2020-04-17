7de2d256661c752c31fd8048d4c153da653f310c
makame <makame@mail.ru>
2020-04-17 23:44:03 +0200

    fixes

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -108,7 +108,7 @@ function createWindow() {
     height: 600,
     minWidth: 800,
     minHeight: 600,
-    fullscreen: process.platform === "darwin",
+    fullscreen: !isWin,
     resizable: true,
     center: true,
     autoHideMenuBar: true,
