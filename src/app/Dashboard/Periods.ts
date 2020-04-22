9c3e9702ba993deec7dbe67aa89d0707fe7565d7
makame <makame@mail.ru>
2020-04-22 16:01:19 +0200

    add additional periods

--- src/app/Dashboard/Periods.ts
+++ src/app/Dashboard/Periods.ts
@@ -5,4 +5,8 @@ export const periods = {
   90: "Last 3 Months",
   180: "Last 6 Months",
   360: "Last Year",
+  720: "Last 2 Years",
+  1080: "Last 3 Years",
+  1800: "Last 5 Years",
+  3600: "Last 10 Years",
 };
