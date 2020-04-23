bb2133ee363969d8758e520a7294be15f4cadf46
makame <makame@mail.ru>
2020-04-23 18:16:24 +0200

    fixes

---
+++ src/app.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./app.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { RoutedContent } from "./routing";
+import { AppLayout } from "./layout/AppLayout";
+import {
+  ServiceProviderFactory,
+  ServiceProviderHook,
+} from "./components/ServiceProvider/ServiceProvider";
+import { AuthService } from "./app/Auth/AuthService";
+import { LoadingScreen } from "./app/Loading/LoadingScreen";
+import { LoadingService } from "./app/Loading/LoadingService";
+import { FavouriteService } from "./app/FavouriteService";
+import { DashboardService } from "./app/DashboardService";
+import { SearchService } from "./app/SearchService";
+import { MessageService } from "./app/MessageService";
+import { RepositoryUserService } from "./app/RepositoryUserService";
+
+const basePath = process.env.BASE_PATH || "/";
+
+export const App = () => {
+  const [ServiceProvider] = React.useState<React.FC>(() =>
+    ServiceProviderFactory(
+      AuthService,
+      LoadingService,
+      FavouriteService,
+      DashboardService,
+      SearchService,
+      MessageService,
+      RepositoryUserService
+    )
+  );
+
+  return (
+    <ServiceProvider>
+      <Router basename={basePath}>
+        <ServiceProviderHook>
+          <LoadingScreen>
+            <AppLayout>
+              <RoutedContent />
+            </AppLayout>
+          </LoadingScreen>
+        </ServiceProviderHook>
+      </Router>
+    </ServiceProvider>
+  );
+};
