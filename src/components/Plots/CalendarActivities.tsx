a33dc6841dbcbf4706221ca9aee59dab9af82b76
makame <makame@mail.ru>
2020-05-16 01:37:46 +0200

    fix activity print

--- src/components/Plots/CalendarActivities.tsx
+++ src/components/Plots/CalendarActivities.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 import moment from "moment";
-import { ResponsiveCalendar } from "@nivo/calendar";
+import { ResponsiveCalendar, Calendar } from "@nivo/calendar";
 import { useMediaQuery } from "react-responsive";
 
 export const CalendarActivities = ({
@@ -44,43 +44,87 @@ export const CalendarActivities = ({
       }}
     >
       <div className="absolute left-0 top-0 bottom-0 right-0">
-        <ResponsiveCalendar
-          data={data}
-          from={past}
-          to={now}
-          maxValue={maxValue || "auto"}
-          minValue={0}
-          emptyColor="#f0eeee"
-          monthBorderWidth={3}
-          monthBorderColor={isDarkMode ? "#aaa" : "#ccc"}
-          dayBorderColor={"#fff"}
-          colors={
-            isDarkMode
-              ? [
-                  "rgba(115, 205, 97, 0.3)",
-                  "rgba(115, 205, 97, 0.4)",
-                  "rgba(115, 205, 97, 0.5)",
-                  "rgba(115, 205, 97, 0.6)",
-                  "rgba(115, 205, 97, 0.7)",
-                  "rgba(115, 205, 97, 0.8)",
-                  "rgba(115, 205, 97, 0.9)",
-                  "rgba(115, 205, 97, 1.0)",
-                ]
-              : [
-                  "rgba(0, 153, 0, 0.3)",
-                  "rgba(0, 153, 0, 0.4)",
-                  "rgba(0, 153, 0, 0.5)",
-                  "rgba(0, 153, 0, 0.6)",
-                  "rgba(0, 153, 0, 0.7)",
-                  "rgba(0, 153, 0, 0.8)",
-                  "rgba(0, 153, 0, 0.9)",
-                  "rgba(0, 153, 0, 1.0)",
-                ]
-          }
-          margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-          yearSpacing={40}
-          dayBorderWidth={2}
-        />
+        {isPrint ? (
+          <Calendar
+            height={height * years}
+            width={990}
+            data={data}
+            isInteractive={!isPrint}
+            from={past}
+            to={now}
+            maxValue={maxValue || "auto"}
+            minValue={0}
+            emptyColor="#f0eeee"
+            monthBorderWidth={3}
+            monthBorderColor={isDarkMode ? "#aaa" : "#ccc"}
+            dayBorderColor={"#fff"}
+            colors={
+              isDarkMode
+                ? [
+                    "rgba(115, 205, 97, 0.3)",
+                    "rgba(115, 205, 97, 0.4)",
+                    "rgba(115, 205, 97, 0.5)",
+                    "rgba(115, 205, 97, 0.6)",
+                    "rgba(115, 205, 97, 0.7)",
+                    "rgba(115, 205, 97, 0.8)",
+                    "rgba(115, 205, 97, 0.9)",
+                    "rgba(115, 205, 97, 1.0)",
+                  ]
+                : [
+                    "rgba(0, 153, 0, 0.3)",
+                    "rgba(0, 153, 0, 0.4)",
+                    "rgba(0, 153, 0, 0.5)",
+                    "rgba(0, 153, 0, 0.6)",
+                    "rgba(0, 153, 0, 0.7)",
+                    "rgba(0, 153, 0, 0.8)",
+                    "rgba(0, 153, 0, 0.9)",
+                    "rgba(0, 153, 0, 1.0)",
+                  ]
+            }
+            margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+            yearSpacing={40}
+            dayBorderWidth={2}
+          />
+        ) : (
+          <ResponsiveCalendar
+            data={data}
+            isInteractive={!isPrint}
+            from={past}
+            to={now}
+            maxValue={maxValue || "auto"}
+            minValue={0}
+            emptyColor="#f0eeee"
+            monthBorderWidth={3}
+            monthBorderColor={isDarkMode ? "#aaa" : "#ccc"}
+            dayBorderColor={"#fff"}
+            colors={
+              isDarkMode
+                ? [
+                    "rgba(115, 205, 97, 0.3)",
+                    "rgba(115, 205, 97, 0.4)",
+                    "rgba(115, 205, 97, 0.5)",
+                    "rgba(115, 205, 97, 0.6)",
+                    "rgba(115, 205, 97, 0.7)",
+                    "rgba(115, 205, 97, 0.8)",
+                    "rgba(115, 205, 97, 0.9)",
+                    "rgba(115, 205, 97, 1.0)",
+                  ]
+                : [
+                    "rgba(0, 153, 0, 0.3)",
+                    "rgba(0, 153, 0, 0.4)",
+                    "rgba(0, 153, 0, 0.5)",
+                    "rgba(0, 153, 0, 0.6)",
+                    "rgba(0, 153, 0, 0.7)",
+                    "rgba(0, 153, 0, 0.8)",
+                    "rgba(0, 153, 0, 0.9)",
+                    "rgba(0, 153, 0, 1.0)",
+                  ]
+            }
+            margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+            yearSpacing={40}
+            dayBorderWidth={2}
+          />
+        )}
       </div>
       <style jsx>{`
         .calendar-activities :global(text) {
