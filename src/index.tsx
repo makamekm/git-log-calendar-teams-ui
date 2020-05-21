f662401565b459b6fa5a22a3286dc704eb456cf7
makame <makame@mail.ru>
2020-05-21 19:47:33 +0200

    fix focused

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles/normalize.scss";
 import "./tailwind.generated.css";
 import "./styles/main.scss";
 import { App } from "./app";
